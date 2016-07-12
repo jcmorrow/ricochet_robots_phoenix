05e5ea3519c38ee7c2c86470303a172680a95cc2
Josh Morrow <jcmorrow@thoughtbot.com>
2016-07-12 07:17:49 -0700

    Configure development database

--- config/dev.exs
+++ config/dev.exs
@@ -36,8 +36,6 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :ricochet_robots_phoenix, RicochetRobotsPhoenix.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "ricochet_robots_phoenix_dev",
   hostname: "localhost",
   pool_size: 10
